8ed55093b8faa212e41fb12265f3365b79c07156
NatMe9 <margo23rest@gmail.com>
2022-05-10 20:08:16 +0300

    first commit)

---
+++ p_storyboard_2/p_storyboard_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  p_storyboard_2
+//
+//  Created by Natalia Givojno on 10.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello")
+    }
+
+
+}
+
