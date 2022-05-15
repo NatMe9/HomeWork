2cf1da816fc0c023b19608fffc15f68d24f10254
NatMe9 <margo23rest@gmail.com>
2022-05-15 23:43:47 +0300

    Update ViewController.swift
    HomeWork 2

--- p_storyboard_2/p_storyboard_2/ViewController.swift
+++ p_storyboard_2/p_storyboard_2/ViewController.swift
@@ -58,8 +58,122 @@ class ViewController: UIViewController {
 //            print ("четное")
 //        }
 //
+        
+        //        1. Универсальные функции сложения, вычитания, умножения и деления (перегрузка функций).
+        minus(a: 8, b: 12)
+        minus(a: 8.0, b: 12.0)
+        minus(a: 8.0, b: 12.0, ctr: 34)
+        minus(a: 1, b: 10, c:200)
+        
+        //        let budjet = convUSD(byn: 10.0, curs: 2.59)
+        //        print(budjet)
+        //        let budjet2 = convUSD(byn: 300.0, curs: 2.59)
+        //        print(budjet2)
+        //        print(budjet2/budjet)
+        
+        //         2. Вычислить сумму цифр четырехзначного числа.
+        var chislo:Int = 1956
+        
+        var total:Int
+        
+        total  = (sum4(p_chi: &chislo))
+        total += (sum4(p_chi: &chislo))
+        total += (sum4(p_chi: &chislo))
+        total += (sum4(p_chi: &chislo))
+        
+        print(total) // сумма цифр четырехзначного числа
+        print(chislo)
+        
+        //        3. Функция сравнения строк – «авб» больше «ввш».
+        
+        
+        srav(p_str1: "авб", p_str2: "ввш")
+        
+//        4. Циклический вызов функций – поломать приложение.
+//        crash()
+//    Результат:Details
+//
+//        Could not attach to pid : “26993”
+//        Domain: IDEDebugSessionErrorDomain
+//        Code: 7
+//        Failure Reason: “HW_2” failed to launch or exited before the debugger could attach to it. Please verify that “HW_2” has a valid code signature that permits it to be launched on “iPhone 11”. Refer to crash logs and system logs to for more diagnostic
+//        print("не поломалось")
+        
+//        5. Функция возведения в степень с дефолтным параметром.
+       
+        vozv(chislo: 3.0)
+        
+//        6. Функция вычисления факториала числа
+        var f:Int = factorial(5)
+        print(f)
+        
     }
 
-
+    //    1. Универсальные функции сложения, вычитания, умножения и деления (перегрузка функций).
+    func minus(a:Int, b:Int) {
+        print(a-b)
+    }
+    func minus(a:Double, b:Double) {
+        print(100 + (a-b))
+    }
+    func minus(a:Double, b:Double, ctr:Double) {
+        print(ctr / (a-b))
+    }
+    func minus(a:Int, b:Int,c:Int) {
+        print(a * b * c)
+    }
+    
+    //    func convUSD(byn:Double, curs:Double) -> Double {
+    //        return(byn * curs)
+    //    }
+    
+    //    2. Вычислить сумму цифр четырехзначного числа.
+    
+    func sum4(p_chi: inout Int) -> Int {
+        let res: Int
+        res = (p_chi) % 10
+        p_chi = Int(p_chi/10)
+        
+        
+        return res
+    }
+//    3. Функция сравнения строк – «авб» больше «ввш».
+    
+    
+    func srav(p_str1:String, p_str2:String){
+        if p_str1>p_str2 {
+            print("первая строка больше второй")
+        }
+        else if p_str1<p_str2{
+            print("вторая строка больше первой")
+        }
+        else {
+            print("строки равны")
+        }
+        
+    }
+    
+//    4. Циклический вызов функций – поломать приложение.
+    
+    func crash(){
+        crash()
+    }
+    
+//    5. Функция возведения в степень с дефолтным параметром.
+    
+    func vozv(chislo:Double, step:Double = 2){
+        var a:Double = pow(Double(chislo),step)
+        print(a)
+    }
+//    6. Функция вычисления факториала числа
+    func factorial(_ chislo:Int) -> Int {
+        if chislo < 1 {
+            return 1
+        } else {
+            return chislo * factorial(chislo-1)
+        }
+   
+    }
+    
 }
 //
