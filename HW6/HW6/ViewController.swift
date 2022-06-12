71ce8ced625741893bef3cdb0ab078c04b5ad7c7
NatMe9 <margo23rest@gmail.com>
2022-06-13 00:08:14 +0300

    HomeWork8(correct HW6)

--- HW6/HW6/ViewController.swift
+++ HW6/HW6/ViewController.swift
@@ -13,13 +13,13 @@ class ViewController: UIViewController {
     
     let centrKrugok = UIView()
     
-    enum peremeschenia {
-        case UP
-        case DOWN
-        case LEFT
-        case RIGHT
-        
-    }
+        enum peremeschenia {
+            case UP
+            case DOWN
+            case LEFT
+            case RIGHT
+    
+        }
     @IBOutlet weak var giveCommand: UILabel!
     
     override func viewDidLoad() {
@@ -27,6 +27,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         centrKrugok.backgroundColor = .purple
+        
+        let rightSwipeGR = UISwipeGestureRecognizer(target: self, action: #selector(rightDvigCircle))
+        rightSwipeGR.direction = .right
+        centrKrugok.addGestureRecognizer(rightSwipeGR)
+        
+        let leftSwipeGR = UISwipeGestureRecognizer(target: self, action: #selector(leftDvigCircle))
+        leftSwipeGR.direction = .left
+        centrKrugok.addGestureRecognizer(leftSwipeGR)
+        
+        let upSwipeGR = UISwipeGestureRecognizer(target: self, action: #selector(UpDvigCircle))
+        upSwipeGR.direction = .up
+        centrKrugok.addGestureRecognizer(upSwipeGR)
+        
+        let downSwipeGR = UISwipeGestureRecognizer(target: self, action: #selector(DownDvigCircle))
+        downSwipeGR.direction = .down
+        centrKrugok.addGestureRecognizer(downSwipeGR)
         
     }
     override func viewWillAppear(_ animated: Bool) {
@@ -40,73 +56,99 @@ class ViewController: UIViewController {
         view.addSubview(centrKrugok)
     }
     
-    @IBAction func UP(_ sender: Any) {
-        movingKrugok(direction: .UP)
-    }
-    @IBAction func DOWN(_ sender: Any) {
-        movingKrugok(direction: .DOWN)
-
-    }
-    @IBAction func LEFT(_ sender: Any) {
-        movingKrugok(direction: .LEFT)
-    }
-    @IBAction func RIGHT(_ sender: Any) {
+    @objc func rightDvigCircle(gesture: UISwipeGestureRecognizer){
         movingKrugok(direction: .RIGHT)
     }
-    func movingKrugok (direction: peremeschenia) {
-        switch direction {
-        case.UP:
-            if centrKrugok.frame.origin.y > 15 {
-                centrKrugok.frame.origin.y -= 15
-                giveCommand.text = "Нажата вверх"
-                giveCommand.backgroundColor = .systemGreen
-            }
-            else {
-                giveCommand.text = "Ошибка!Вверх больше нельзя"
-                giveCommand.backgroundColor = .red
-                giveCommand.textColor =  .white
-            }
-        case .DOWN:
-            if centrKrugok.frame.origin.y < view.frame.maxY - 450 {
-                centrKrugok.frame.origin.y += 20
-                giveCommand.text = "Нажата вниз"
-                giveCommand.backgroundColor = .systemGreen
-            }
-            else {
-                giveCommand.text = "Ошибка!Вниз больше нельзя"
-                giveCommand.backgroundColor = .red
-                giveCommand.textColor =  .white
-            }
-        case.LEFT:
-            if centrKrugok.frame.origin.x > 15 {
-                centrKrugok.frame.origin.x -= 20
-                giveCommand.text = "Нажата влево"
-                giveCommand.backgroundColor = .systemGreen
-            }
-            else {
-                giveCommand.text = "Ошибка!Влево больше нельзя"
-                giveCommand.backgroundColor = .red
-                giveCommand.textColor =  .white
-            }
-        case .RIGHT:
-            if centrKrugok.frame.origin.x < view.frame.maxX - centrKrugok.frame.width - 15 {
-                centrKrugok.frame.origin.x += 20
-                giveCommand.text = "Нажата вправо"
-                giveCommand.backgroundColor = .systemGreen
-                
-            }
-            else {
-                giveCommand.text = "Ошибка!Вправо больше нельзя"
-                giveCommand.backgroundColor = .red
-                giveCommand.textColor =  .white
-            }
-        }
-        
-        
-        
-        
+    
+    @objc func leftDvigCircle(gesture: UISwipeGestureRecognizer){
+        movingKrugok(direction: .LEFT)
     }
+    
+    @objc func UpDvigCircle(gesture: UISwipeGestureRecognizer){
+        movingKrugok(direction: .UP)
+    }
+    
+    @objc func DownDvigCircle(gesture: UISwipeGestureRecognizer){
+        movingKrugok(direction: .DOWN)
+    }
+    
+    
+    
+    
+    
+        @IBAction func UP(_ sender: Any) {
+          movingKrugok(direction: .UP)
+    
+        }
+        @IBAction func DOWN(_ sender: Any) {
+            movingKrugok(direction: .DOWN)
+    
+    
+        }
+        @IBAction func LEFT(_ sender: Any) {
+            movingKrugok(direction: .LEFT)
+    
+        }
+        @IBAction func RIGHT(_ sender: Any) {
+            movingKrugok(direction: .RIGHT)
+    
+        }
+    
+        func movingKrugok (direction: peremeschenia) {
+            switch direction {
+            case.UP:
+                if centrKrugok.frame.origin.y > 15 {
+                    centrKrugok.frame.origin.y -= 15
+                    giveCommand.text = "Вверх"
+                    giveCommand.backgroundColor = .systemGreen
+                }
+                else {
+                    giveCommand.text = "Ошибка!Вверх больше нельзя"
+                    giveCommand.backgroundColor = .red
+                    giveCommand.textColor =  .white
+                }
+            case .DOWN:
+                if centrKrugok.frame.origin.y < view.frame.maxY - 450 {
+                    centrKrugok.frame.origin.y += 20
+                    giveCommand.text = "Вниз"
+                    giveCommand.backgroundColor = .systemGreen
+                }
+                else {
+                    giveCommand.text = "Ошибка!Вниз больше нельзя"
+                    giveCommand.backgroundColor = .red
+                    giveCommand.textColor =  .white
+                }
+            case.LEFT:
+                if centrKrugok.frame.origin.x > 15 {
+                    centrKrugok.frame.origin.x -= 20
+                    giveCommand.text = "Влево"
+                    giveCommand.backgroundColor = .systemGreen
+                }
+                else {
+                    giveCommand.text = "Ошибка!Влево больше нельзя"
+                    giveCommand.backgroundColor = .red
+                    giveCommand.textColor =  .white
+                }
+            case .RIGHT:
+                if centrKrugok.frame.origin.x < view.frame.maxX - centrKrugok.frame.width - 15 {
+                    centrKrugok.frame.origin.x += 20
+                    giveCommand.text = "Вправо"
+                    giveCommand.backgroundColor = .systemGreen
+    
+                }
+                else {
+                    giveCommand.text = "Ошибка!Вправо больше нельзя"
+                    giveCommand.backgroundColor = .red
+                    giveCommand.textColor =  .white
+                }
+            }
+    
+    
+    
+    
+        }
     
     
 }
+
 
