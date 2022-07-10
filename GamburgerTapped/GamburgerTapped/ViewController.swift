7f5f9c9d4ac2786a5fdd99449872711682222efc
NatMe9 <margo23rest@gmail.com>
2022-07-11 00:21:10 +0300

    GamburgerTapped(with allert)

--- GamburgerTapped/GamburgerTapped/ViewController.swift
+++ GamburgerTapped/GamburgerTapped/ViewController.swift
@@ -8,11 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     let blurEffect = UIBlurEffect(style: .dark)
     let blurEffectView = UIVisualEffectView()
-    
-
     
     @IBOutlet weak var leading: NSLayoutConstraint!
     @IBOutlet weak var trailing: NSLayoutConstraint!
@@ -23,13 +21,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-//        let blurEffect = UIBlurEffect(style: .dark)
-//        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        
         blurEffectView.effect = blurEffect
         blurEffectView.tag = 100
         blurEffectView.frame = view.bounds
     }
-
+    
     @IBAction func gambTap(_ sender: Any) {
         
         if gambOut == false {
@@ -37,27 +34,58 @@ class ViewController: UIViewController {
             trailing.constant = -180
             gambOut = true
             
-            
-            
-    
-//            view.insertSubview(blurEffectView, at: 0)
             fon.addSubview(blurEffectView)
-        
+            
         } else {
             leading.constant = 0
             trailing.constant = 0
             gambOut = false
             
             if let viewWithTag = fon.viewWithTag(100) {
-                    viewWithTag.removeFromSuperview()
-                }else{
-                    print("No!")
-                }
+                viewWithTag.removeFromSuperview()
+            }else{
+                print("No!")
+            }
             
         }
         
         
     }
     
+    @IBAction func autorisaze(_ sender: Any) {
+        
+    }
+    
+    @IBAction func showAlertOk(_ sender: Any) {
+        
+        shAlert(title: "AlERT", message: "Test alert!!!" , actionTitles: ["Ok"], actions: [ { action1 in
+        }, { action2 in
+        }])
+        
+    }
+    
+
+    @IBAction func showAlert(_ sender: Any) {
+        
+        shAlert(title: "AlERT", message: "Test alert!!!" , actionTitles: ["Ok","Cancel"], actions: [ { action1 in
+        }, { action2 in
+        }])
+    
+        
+    }
+    
+    
 }
 
+extension UIViewController {
+    
+    func shAlert(title: String?, message: String?, actionTitles:[String?], actions:[((UIAlertAction) -> Void)?]) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        for (index, title) in actionTitles.enumerated() {
+            let action = UIAlertAction(title: title, style: .default, handler: actions[index])
+            alert.addAction(action)
+        }
+        present(alert, animated: true, completion: nil)
+    }
+    
+}
